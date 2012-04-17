9fe47760f561ceae5d52a552828382a72b426e72
Zach Young <zach@thisisgrow.com>
2012-04-17 12:08:15 -0400

    updated fixop.sh so it would work on entire src tree. git ignore ignores backup files it creates

--- misc/fixop.sh
+++ misc/fixop.sh
@@ -1,28 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 
 fixop() {
-  for f in *.c; do
-    [[ -f $f ]] || continue
-    cp "$f" "$f~"
-  done
 
-  perl -pi -w -e 's/(\s)EQ(\s)/$1\=\=$2/g;' *.c
-  perl -pi -w -e 's/(\s)NE(\s)/$1\!\=$2/g;' *.c
-  perl -pi -w -e 's/(\s)GE(\s)/$1\>\=$2/g;' *.c
-  perl -pi -w -e 's/(\s)LE(\s)/$1\<\=$2/g;' *.c
-  perl -pi -w -e 's/(\s)AND(\s)/$1\&\&$2/g;' *.c
-  perl -pi -w -e 's/(\s)OR(\s)/$1\|\|$2/g;' *.c
-  perl -pi -w -e 's/(\s)GT(\s)/$1\>$2/g;' *.c
-  perl -pi -w -e 's/(\s)LT(\s)/$1\<$2/g;' *.c 
-  perl -pi -w -e 's/(\s)NOT(\s)/$1\!$2/g;' *.c
-  perl -pi -w -e 's/(\()NOT(\s)/$1\!$2/g;' *.c
+	for f in `find ../src -iname *.c`
+	do
+		echo "Fixing ${f}"
+		cp ${f} ${f}~
+
+		#for f in ${f}; do
+		#  [[ -f $f ]] || continue
+		#  cp "$f" "$f~"
+		#done
+
+		perl -pi -w -e 's/(\s)EQ(\s)/$1\=\=$2/g;' ${f}
+		perl -pi -w -e 's/(\s)NE(\s)/$1\!\=$2/g;' ${f}
+		perl -pi -w -e 's/(\s)GE(\s)/$1\>\=$2/g;' ${f}
+		perl -pi -w -e 's/(\s)LE(\s)/$1\<\=$2/g;' ${f}
+		perl -pi -w -e 's/(\s)AND(\s)/$1\&\&$2/g;' ${f}
+		perl -pi -w -e 's/(\s)OR(\s)/$1\|\|$2/g;' ${f}
+		perl -pi -w -e 's/(\s)GT(\s)/$1\>$2/g;' ${f}
+		perl -pi -w -e 's/(\s)LT(\s)/$1\<$2/g;' ${f} 
+		perl -pi -w -e 's/(\s)NOT(\s)/$1\!$2/g;' ${f}
+		perl -pi -w -e 's/(\()NOT(\s)/$1\!$2/g;' ${f}
+	done
 }
 
 while true; do
-    read -p "Change all operator macros (EQ,NE,GE...) to real operators (==,!=,>=) in *.c in current directory? y/n : " yn
-    case $yn in
-        [Yy]* ) fixop; echo "Done."; exit;;
-        [Nn]* ) return;;
-        * ) echo "Please answer y or n.";;
-    esac
+	read -p "Change all operator macros (EQ,NE,GE...) to real operators (==,!=,>=) in the src directory? y/n : " yn
+	case ${yn} in
+		[Yy]* ) fixop; echo "Done."; exit;;
+		[Nn]* ) return;;
+		* ) echo "Please answer y or n.";;
+	esac
 done
